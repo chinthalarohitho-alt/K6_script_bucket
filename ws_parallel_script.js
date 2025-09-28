b84bc9ba31f24641cd7f5b70616d65bcaecaa2d1
rohithkumaro <rohith.kumar@yellow.ai>
2025-09-28 19:53:18 +0530

    removed the console line

--- ws_parallel_script.js
+++ ws_parallel_script.js
@@ -14,21 +14,21 @@ const urls = [
 
 export default function () {
   const url = urls[(__VU - 1) % urls.length];
-  // console.log(`VU: ${__VU} -> url: ${url}`);
+
   const res = ws.connect(url, {}, function (socket) {
-    // console.log(url);
+
     socket.on('open', function () {
       socket.send('Hello from k6!');
     });
     socket.on('message', function (msg) {
-      // console.log('Received:', msg);
+      console.log('Received:', msg);
       socket.close();
     });
     socket.on('close', function () {
-      // console.log('Connection closed');
+      console.log('Connection closed');
     });
     socket.on('error', function (e) {
-      // console.error('Error:', e.error());
+      console.error('Error:', e.error());
     });
   });
   check(res, { 'status is 101': (r) => r && r.status === 101 });
